efb3d99a59307887a985cad83b550cf635dae110
M.S <mixa.ru.ru@gmail.com>
2021-10-11 08:41:43 +0300

    earthquakes fetcher

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 import L from "leaflet";
 
 const QUAKE_URL = `http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojsonp`;
@@ -18,3 +18,17 @@ document.body.appendChild(mapContainer);
 
 const map = L.map("map").setView([33.858631, -118.279602], 7);
 L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
+
+const quakes$ = new Observable((observer) => {
+    window.eqfeed_callback = response => {
+        response.features.forEach(observer.next);
+    };
+    loadJSONP(QUAKE_URL);
+});
+
+quakes$.subscribe(quake => {
+    const coords = quake.geometry.coordinates;
+    const size = quake.properties.mag * 10000;
+
+    L.circle([coords[1],coords[0]], size).addTo(map);
+});

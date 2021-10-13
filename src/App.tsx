ba78d715bcb556e3cb3064874b1b9344d0907ab3
M.S <mixa.ru.ru@gmail.com>
2021-10-13 08:23:59 +0300

    subscribe for data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { rawEarthQuake$ } from './store';
+import React, { useEffect } from 'react';
 import './App.css';
 
+import { rawEarthQuake$ } from './store';
+
 function App() {
+
+  useEffect(() => {
+    rawEarthQuake$.subscribe(console.log);
+  });
+
   return (
     <div>
     </div>
